0b1b09c6258611f4982df34f77d541ffc71c583b
shivansh <shivresides@gmail.com>
2025-06-27 13:15:53 +0530

    auth done

--- apps/http-backend/src/routes/auth.ts
+++ apps/http-backend/src/routes/auth.ts
@@ -2,7 +2,8 @@ import express, { Router } from "express"
 import jwt from "jsonwebtoken"
 import {prisma} from "@repo/db-package/client"
 import {userSchema} from "@repo/common/types"
-
+import bcrypt from "bcrypt"
+import {JWT_SIGN} from "@repo/backend-common/config"
 
 const userRouter : Router = express.Router()
 
@@ -15,11 +16,12 @@ userRouter.post("/signup", async (req, res)=> {
         })
         return
     }
+    const hashedPassword = await bcrypt.hash(parsedData.data.password, 5);
     try {
         const user = await prisma.user.create({
             data : {
                 name : parsedData.data.username,
-                password : parsedData.data.password,    
+                password : hashedPassword,   
                 email : parsedData.data.email
             }
         } )
@@ -34,31 +36,42 @@ userRouter.post("/signup", async (req, res)=> {
     
     })
 
-// userRouter.post("/signin", async (req, res)=> {
-//     const username = req.body.username;
-//     const password = req.body.password;
-//     try {
-//     const user = await prisma.user.findOne({
-//         data : {
-//             username : username,
-//             password : password
-//         }
-//     })
-//     if (user){
-//     const token = jwt.sign({
-//         id : user.id
-//     }, JWT_SIGN)
+userRouter.post("/signin", async (req, res)=> {
+    const parsedData = userSchema.safeParse(req.body)
+    const email = parsedData.data?.email
+    
+    const user = await prisma.user.findFirst({
+        where : {email}
+    })
+    
 
-//     res.status(200).json({
-//         token,
-//         message : "Signed Up"
-//     })}} 
-//     catch {
-//     res.status(400).json({
-//         message : "bad request"
-//     })
-//     }}
-
-// )
+    if (!user) {
+         res.status(404).json({ error: "User not found" });
+         return;
+      }
+    if (typeof(parsedData.data?.password) !== "string" ){
+        res.status(411).json({
+            message : "something went wrong"
+        })
+        return ;
+    }
+    const decoded = await bcrypt.compare(parsedData.data?.password, user.password)
+    if (decoded) {
+        const token = jwt.sign({
+            userId : user.id
+        }, JWT_SIGN)
+    
+        res.status(200).json({
+            token,
+            message : "Signed Up"
+        }
+    )
+    } else {
+        res.status(403).json({
+            message : "Bad Request"
+        })
+    }
+}
+)
 
 export default userRouter
