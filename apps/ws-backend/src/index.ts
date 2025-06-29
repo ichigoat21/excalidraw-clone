ea32ab4b998919e2e375391046d648ca3637f792
shivansh <shivresides@gmail.com>
2025-06-30 01:52:23 +0530

    ws ep tested

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,17 +1,18 @@
 import {WebSocketServer} from "ws"
-import jwt, { JwtPayload } from "jsonwebtoken"
+import jwt from "jsonwebtoken"
 import {JWT_SIGN} from "@repo/backend-common/config"
-const wss = new WebSocketServer({port : 8000})
+const wss = new WebSocketServer({port : 8001})
 
 
 function checkUser(token : string) : string | null {
-    const decoded = jwt.verify(JWT_SIGN, token as string)
+    console.log("token: ", token)
+    const decoded = jwt.verify(token, JWT_SIGN)
 
     if (typeof(decoded) == "string"){
         return null;
     }
 
-    if (!decoded ||decoded.userId){
+    if (!decoded || !decoded.userId){
        return null;
     }
     
@@ -28,12 +29,13 @@ wss.on("connection", function connection(ws, request) {
     const token = queryParams.get('token')
 
     if (typeof(token) !== "string"){
+        ws.close()
         return;
     }
     const userId = checkUser(token)
-
     if(!userId){
         ws.close()
+        return;
     }
     ws.on("message", function message(){
        ws.send("pong")
