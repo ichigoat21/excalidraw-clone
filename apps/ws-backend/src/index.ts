4ba46c87768e4e960eff71822e080c395b17fdcc
shivansh <shivresides@gmail.com>
2025-10-13 04:30:05 +0530

    verification in ws-backend

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,11 +1,26 @@
 import { WebSocketServer } from "ws";
+import { JWT_SECRET } from "@repo/config/config";
+import jwt, { JwtPayload } from 'jsonwebtoken'
+
+
 
 
 const wss = new WebSocketServer({
     port : 8080
 })
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws, request) {
+    const url = request.url;
+
+    const queryParams = new URLSearchParams(url?.split('?')[1])
+    const token = queryParams.get('token')
+    const decoded = jwt.verify(token as string, JWT_SECRET)
+
+    if(!decoded || !(decoded as JwtPayload).id){
+        ws.close()
+        return
+    }
+
 
     ws.on('message', function message(data){
         ws.send('pong')
