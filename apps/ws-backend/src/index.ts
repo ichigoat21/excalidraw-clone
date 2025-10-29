254b54e52245b879d51e0fb443417df92cf9cb55
shivansh <shivresides@gmail.com>
2025-10-29 18:35:46 +0530

    ws-layer-done

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,11 +1,18 @@
 import { WebSocketServer } from "ws";
+import type WebSocket from "ws";
 import { JWT_SECRET } from "@repo/config/config";
 import jwt, { JwtPayload } from 'jsonwebtoken'
+import { client } from "@repo/db-package/client";
 
 const wss = new WebSocketServer({
     port : 8080
 })
 
+interface User {
+    ws : WebSocket,
+    rooms : [],
+    userId : string
+}
 const decoded = (token: string): string | null => {
     try {
       const decodedToken = jwt.verify(token, JWT_SECRET)
@@ -20,6 +27,8 @@ const decoded = (token: string): string | null => {
     }
   }
 
+  const users : User[] = []
+
 wss.on('connection', function connection(ws, request) {
 
     const url = request.url;
@@ -29,15 +38,47 @@ wss.on('connection', function connection(ws, request) {
         ws.close()
         return
     }
-    const userAuthenticated = decoded(token)
-    if (!userAuthenticated){
+    const userId = decoded(token)
+    if (!userId){
         ws.close()
         return
     }
-    
+
+    users.push({
+        userId,
+        ws,
+        rooms : []
+    })
 
 
-    ws.on('message', function message(data){
-        ws.send('pong')
+
+    ws.on('message', async function message(data){
+        if (typeof data !== 'string'){
+            ws.close()
+            return
+        }
+        const parsedData = JSON.parse(data)
+        if(parsedData.type === "join"){
+            const user = users.find(u => u.ws === ws)
+        }
+        if (parsedData.type === "leave"){
+            const user = users.filter(u => u.ws === ws)
+        }
+        if (parsedData.type === "chat"){
+            users.forEach(user => {
+                user.ws.send(JSON.stringify({
+                    type : "chat",
+                    message : parsedData.message,
+                    rooms : parsedData.roomId
+                }))
+            })
+        }
+        await client.chat.create({
+            data : {
+                message : parsedData.message,
+                roomId : Number(parsedData.roomId),
+                userId : userId
+            }
+        })
     })
 })
