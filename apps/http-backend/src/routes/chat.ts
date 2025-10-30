2e499452f1fa4a90a0a36f177b9320d04910b957
shivansh <shivresides@gmail.com>
2025-10-30 13:17:17 +0530

    ws-layer-completed

--- apps/http-backend/src/routes/chat.ts
+++ apps/http-backend/src/routes/chat.ts
@@ -6,32 +6,60 @@ const roomRouter = express.Router()
 
 
 roomRouter.post("/chat", async (req, res)=> {
-    const parsedData = roomSchema.safeParse(req.body)
+    try {
+        const parsedData = roomSchema.safeParse(req.body)
 
-    if (!parsedData.success){
-        res.status(403).json({
-            message : 'Invalid Inputs'
-        })
-        return
-    }
-    const userId = req.id
-
-    if (typeof userId !== "string") {
-        res.status(403).json({
-            message : 'Sorry something went wrong'
-        })
-        return;
-      }
-
-    const room = await client.room.create({
-        data : {
-            slug : parsedData.data.name,
-            adminId : userId
+        if (!parsedData.success){
+            res.status(403).json({
+                message : 'Invalid Inputs'
+            })
+            return
         }
-    })
+        const userId = req.id
+    
+        if (typeof userId !== "string") {
+            res.status(403).json({
+                message : 'Sorry something went wrong'
+            })
+            return;
+          }
+    
+        const room = await client.room.create({
+            data : {
+                slug : parsedData.data.name,
+                adminId : userId
+            }
+        })
+    
+        res.status(200).json({
+            message : 'Succesfully joined the room',
+            roomId : room.id
+        })
+    } catch {
+        res.status(411).json({
+            message : 'Error Creating Chat'
+        })
+    }
+    
+})
 
-    res.status(200).json({
-        message : 'Succesfully joined the room',
-        roomId : room.id
-    })
+roomRouter.get("/chat/:id", async (req, res) => {
+    try {
+      const roomId = Number(req.params.id);
+      const messages = await client.chat.findMany({
+        where : {
+            roomId : roomId
+        }, orderBy : {
+            id : "desc",
+        }, take : 50
+      })
+
+      res.status(200).json({
+        messages
+      })
+    } catch (e){
+        res.status(411).json({
+            message : 'Error Finding chats'
+        })
+    }
 })
