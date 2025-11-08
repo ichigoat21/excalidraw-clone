77e4a889e003096e210617d975eacc2cd254ff92
shivansh <shivresides@gmail.com>
2025-11-08 13:44:19 +0530

    added buttons

--- apps/web/app/components/Canvas.tsx
+++ apps/web/app/components/Canvas.tsx
@@ -1,12 +1,15 @@
 "use client"
 
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import { initDraw } from "../draw/draw"
+import { IconButton } from "./IconButton";
+import { Circle, Pencil, RectangleCircleIcon, RectangleHorizontal } from "lucide-react";
 
 export type Tool = "circle" | "rect" | "pencil";
 
 export default function Canvas({roomId, socket} : {roomId : string, socket : WebSocket}){
     const canvasRef = useRef<HTMLCanvasElement>(null)
+    const [selectedTool, setSelectedTool] = useState<Tool>("pencil")
 
     useEffect(() => {
         if (canvasRef.current) {
@@ -21,5 +24,28 @@ export default function Canvas({roomId, socket} : {roomId : string, socket : Web
 
     return <div>
         <canvas ref={canvasRef}></canvas>
+        <TopBar selectedTool={selectedTool} setSelectedTool={setSelectedTool}/>
     </div>
+}
+
+function TopBar({
+  selectedTool,
+  setSelectedTool
+} : {
+  selectedTool : Tool,
+  setSelectedTool : (s : Tool) => void
+}){
+  return <div className="fixed top-10 left-10">
+    <div className="flex"> 
+    <IconButton icon={<Pencil/>} onClick={()=> {
+      setSelectedTool("pencil")
+    }} activated={selectedTool==='pencil'}/>
+    <IconButton icon={<RectangleHorizontal/>} onClick={()=> {
+      setSelectedTool("rect")
+    }} activated={selectedTool === 'rect'}/>
+    <IconButton icon={<Circle/>}  onClick={()=> {
+      setSelectedTool("circle")
+    }} activated={selectedTool === 'circle'}/>
+    </div>
+  </div>
 }
