5a2bc618401cd205a234311fd54529d865b25770
shivansh <shivresides@gmail.com>
2025-12-05 15:59:53 +0530

    fe optimized

--- apps/web/app/canvas/[roomID]/page.tsx
+++ apps/web/app/canvas/[roomID]/page.tsx
@@ -1,10 +1,13 @@
-import RoomCanvas from "@/app/components/RoomCanvas"
+import RoomCanvas from "@/app/components/RoomCanvas";
 
-
-export default async function CanvasPage({ params }: { params: { roomID: string } }){
-     const param = (await params).roomID
-    
-
-     return <RoomCanvas roomId={param}/>
-
-}
+export default async function Page({
+     params,
+   }: {
+     params: { roomID: string } | Promise<{ roomID: string }>;
+   }) {
+     const resolved = await params;
+     const roomID = resolved.roomID;
+   
+     return <RoomCanvas roomId={roomID} />;
+   }
+   
